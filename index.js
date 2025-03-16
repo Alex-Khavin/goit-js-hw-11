16082b0b62a3d2f5f3cebbdf56bfa3d94d1946f2
Alex-Khavin <alexandr_havin@yahoo.com>
2025-03-16 16:42:58 +0000

    Deploying to gh-pages from @ Alex-Khavin/goit-js-hw-11@22afce2864861c008fdc638827a0c9b32d5172d2 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import"./assets/styles-B3di-5fm.js";import{i as n,S as c,a as m}from"./assets/vendor-DtRopbQG.js";let o="";const i={params:{key:"49358798-a0fde913d86352b572e9384bf",q:"",image_type:"photo",orientation:"horizontal",safesearch:!0}},g=document.querySelector(".form"),u=document.querySelector("input"),s=document.querySelector(".gallery"),l=document.querySelector(".loader");g.addEventListener("submit",h);u.addEventListener("input",p);document.addEventListener("DOMContentLoaded",()=>{l.classList.add("hidden")});function p(t){o=t.target.value.trim()}function h(t){t.preventDefault();const e=t.currentTarget;if(o===""){n.show({message:"Fill in the search field!",messageColor:"white",iconUrl:"./img/cancel-circle.svg",position:"topRight",color:"#ef4040"});return}i.params.q=o,s.innerHTML="",l.classList.remove("hidden"),f(i).then(a=>{s.insertAdjacentHTML("beforeend",y(a)),d.refresh()}),e.reset(),o=""}let d=new c(".gallery a",{captionsData:"alt",captionDelay:250});function f(t){return m.get("https://pixabay.com/api/?",t).then(e=>{var a,r;if(!((r=(a=e.data)==null?void 0:a.hits)!=null&&r.length))throw new Error("Sorry, there are no images matching your search query. Please try again!");return e.data.hits}).catch(e=>(n.show({message:`${e.message}`,messageColor:"white",iconUrl:"./img/cancel-circle.svg",position:"topRight",color:"#ef4040"}),[]))}function y(t){return t.map(e=>`<li class="gallery-item">
+import{i as u,S as d,a as f}from"./assets/vendor-DtRopbQG.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function i(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(t){if(t.ep)return;t.ep=!0;const r=i(t);fetch(t.href,r)}})();let n="";const l={params:{key:"49358798-a0fde913d86352b572e9384bf",q:"",image_type:"photo",orientation:"horizontal",safesearch:!0}},p=document.querySelector(".form"),g=document.querySelector("input"),c=document.querySelector(".gallery"),m=document.querySelector(".loader");p.addEventListener("submit",y);g.addEventListener("input",h);document.addEventListener("DOMContentLoaded",()=>{m.classList.add("hidden")});function h(o){n=o.target.value.trim()}function y(o){o.preventDefault();const e=o.currentTarget;if(n===""){u.show({message:"Fill in the search field!",messageColor:"white",iconUrl:"./img/cancel-circle.svg",position:"topRight",color:"#ef4040"});return}l.params.q=n,c.innerHTML="",m.classList.remove("hidden"),L(l).then(i=>{c.insertAdjacentHTML("beforeend",w(i)),b.refresh()}),e.reset(),n=""}let b=new d(".gallery a",{captionsData:"alt",captionDelay:250});function L(o){return f.get("https://pixabay.com/api/?",o).then(e=>{var i,s;if(!((s=(i=e.data)==null?void 0:i.hits)!=null&&s.length))throw new Error("Sorry, there are no images matching your search query. Please try again!");return e.data.hits}).catch(e=>(u.show({message:`${e.message}`,messageColor:"white",iconUrl:"./img/cancel-circle.svg",position:"topRight",color:"#ef4040"}),[]))}function w(o){return o.map(e=>`<li class="gallery-item">
     <a class="gallery-link" href="${e.largeImageURL}">
     <img
     class="gallery-image"
@@ -6,9 +6,11 @@ import"./assets/styles-B3di-5fm.js";import{i as n,S as c,a as m}from"./assets/ve
     alt="${e.tags}"
     />
     </a>
-    <p class="item-text">Likes: ${e.likes}</p>
-    <p class="item-text">Views: ${e.views}</p>
-    <p class="item-text">Comments: ${e.comments}</p>
-    <p class="item-text">Downloads: ${e.downloads}</p>
+    <div class="item-subtitle">
+    <p class="item-text"><b>Likes</b> ${e.likes}</p>
+    <p class="item-text"><b>Views</b> ${e.views}</p>
+    <p class="item-text"><b>Comments</b> ${e.comments}</p>
+    <p class="item-text"><b>Downloads</b> ${e.downloads}</p>
+    </div>
     </li>`).join("")}
 //# sourceMappingURL=index.js.map
